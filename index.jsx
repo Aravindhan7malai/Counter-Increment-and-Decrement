a0bcae8d513d57fcca69bcdbbfe316eba8a2e933
Aravindhan Elumalai <159620206+Aravindhan7malai@users.noreply.github.com>
2024-06-12 16:07:58 +0530

    Create index.jsx

---
+++ index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Head from './App.jsx'
+import './style.css'
+ const root=ReactDOM.createRoot(document.getElementById('root'))
+
+ 
+root.render(
+  <>
+  <div className='container'>
+   <Head/>
+  </div>
+ 
+  </>
+)
+  
+
+
